17ac1fd366451ea2041043055fbe855c75a7e880
jeckbjy <jeckbjy@163.com>
2016-09-14 01:01:40 +0800

    dotnet

--- lib-csharp/Decoder.cs
+++ lib-csharp/Decoder.cs
@@ -1,212 +1,139 @@
 ﻿using System;
 using System.IO;
 using System.Text;
-using System.Reflection;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace proto
 {
-    public class Decoder
+    class Decoder
     {
-        private List<uint> m_indexs = new List<uint>();
-        private Stream m_stream;
-        // flag中读取到的当前数据
+        private MemoryStream m_stream;
+        private long m_epos;
+        private uint m_tag = 0;
+
         private ulong m_val;
-        private uint m_tag;
-        private bool m_ext;
-        public Decoder(Stream stream)
+        private bool  m_ext;
+
+        private bool EndOfStream
+        {
+            get { return m_stream.Position >= m_epos; }
+        }
+
+        Decoder(MemoryStream stream)
         {
             m_stream = stream;
+            m_epos = stream.Length;
         }
 
-        public T Decode<T>() where T:class
+        public bool Decode(IMessage msg)
         {
-            T msg = (T)Decode();
-            return msg;
-        }
-
-        public object Decode()
-        {
-            // clear
-            m_indexs.Clear();
-            //
-            long pos = m_stream.Position;
-            uint flag = (uint)m_stream.ReadByte();
-            uint len1, len2, len3, len;
-            len1 = (flag >> 5);
-            len2 = (flag >> 2) & 0x07;
-            len3 = flag & 0x03;
-            len = len1 + len2 + len3;
-            byte[] buff = new byte[len];
-            if (m_stream.Read(buff, 0, (int)len) < len)
-                return null;
-            uint msg_len, idx_len, msg_id;
-            msg_len = decode_group_var(buff, 0, len1);
-            idx_len = decode_group_var(buff, len1, len2);
-            msg_id  = decode_group_var(buff, len1 + len2, len3);
-            if (m_stream.Length - m_stream.Position < msg_len)
-            {
-                m_stream.Seek(pos, SeekOrigin.Begin);
-                return null;
-            }
-            // 校验消息
-            object msg = ModelManager.Create(msg_id);
-            if(msg == null)
-                return null;
-            // 开始解析消息
-            pos = m_stream.Position;
-            long epos = pos + msg_len;
-            long offset = msg_len - idx_len;
-            if(idx_len > 0)
-            {// 先读取index
-                uint val;
-                m_stream.Seek(offset, SeekOrigin.Current);
-                while(m_stream.Position < epos)
-                {
-                    val = (uint)ReadVariant();
-                    m_indexs.Add(val);
-                }
-                m_stream.Seek(pos, SeekOrigin.Begin);
-            }
-            if(msg_len > 0)
-            {// 解析消息
-                ReadPacket(msg, (uint)offset);
-            }
-            // 移动到合适位置,防止有错误消息解析
-            m_stream.Seek(pos + msg_len, SeekOrigin.Begin);
-
-            return msg;
-        }
-
-        private void ReadPacket(object msg, uint lens)
-        {
-            MetaType meta = ModelManager.GetMeta(msg.GetType());
-            if (meta == null)
-                return;
-            uint field_tag = 0;
-            long field_epos;
-            long epos = m_stream.Position + lens;
-            while(m_stream.Position < epos)
-            {
-                ReadTag();
-                field_tag += m_tag;
-                field_epos = m_stream.Position;
-                if (m_ext)
-                    field_epos += (long)m_val;
-                MetaField field_meta = meta.GetField(field_tag);
-                if (field_meta != null)
-                {// 读取field
-                    object field = ReadField(field_meta.Type);
-                    field_meta.SetValue(msg, field);
-                }
-                // 忽略并校验位置
-                m_stream.Seek(field_epos, SeekOrigin.Begin);
-            }
+            return true;
         }
 
         private object ReadField(Type type)
         {
+            object result = null;
+            if(m_ext)
+            {
+                long epos = m_epos;
+                m_epos = m_stream.Position + (long)m_val;
+                result = ReadObject(type);
+                m_epos = epos;
+            }
+            else if(type.IsValueType)
+            {
+                result = ReadObject(type);
+            }
+
+            return result;
+        }
+
+        private object ReadObject(Type type)
+        {
             if (type.IsValueType)
             {
-                return decode(type, m_val);
+                return Converter.decode(type, m_val);
             }
-            else if (type.IsGenericType)
-            {
-                return ReadGeneric(type);
-            }
-            else if(type == typeof(string))
+            else if (type == typeof(string))
             {
                 byte[] buff = new byte[m_val];
-                if (m_stream.Read(buff, 0, (int)m_val) < (int)m_val)
-                    return null;
-                string str = Encoding.UTF8.GetString(buff);
-                return str;
+                if (Read(buff, (int)m_val))
+                    return Encoding.UTF8.GetString(buff);
             }
-            else if(type == typeof(MemoryStream))
+            else if (type == typeof(MemoryStream))
             {
                 MemoryStream stream = new MemoryStream();
-                stream.SetLength((long)m_val);
-                if (m_stream.Read(stream.GetBuffer(), 0, (int)m_val) < (int)m_val)
-                    return null;
-                return stream;
+                stream.SetLength((int)m_val);
+                if (Read(stream.GetBuffer(), (int)m_val))
+                    return stream;
             }
-            else if(type.IsClass)
-            {// 复杂类型
-                object msg = ModelManager.Create(type);
-                if (msg == null)
-                    return null;
-                ReadPacket(msg, (uint)m_val);
+            else if (typeof(IMessage).IsAssignableFrom(type))
+            {// 消息解析
+                IMessage msg = (IMessage)NewObject(type);
+                msg.Decode(this);
                 return msg;
             }
+            else if (type.IsGenericType)
+            {// 泛型解析
+                Type type_def = type.IsGenericTypeDefinition ? type : type.GetGenericTypeDefinition();
+                // LinkedList:暂时无法支持
+                Type[] gen_types = type.GetGenericArguments();
+                object container = NewObject(type);
+
+                if (typeof(IList).IsAssignableFrom(type_def))
+                {
+                    IList list = (IList)container;
+                    while (!EndOfStream)
+                    {
+                        object obj = ReadObject(gen_types[0]);
+                        if (obj != null)
+                            list.Add(obj);
+                    }
+                }
+                else if (typeof(IDictionary<,>).IsAssignableFrom(type_def))
+                {
+                    IDictionary dict = (IDictionary)container;
+                    while (!EndOfStream)
+                    {
+                        object key = ReadObject(gen_types[0]);
+                        object val = ReadObject(gen_types[1]);
+                        if (key != null && val != null)
+                            dict.Add(key, val);
+                    }
+                }
+
+                return container;
+            }
+
             return null;
         }
 
-        private object ReadGeneric(Type type)
+        private object NewObject(Type type)
         {
-            Type type_def = type.IsGenericTypeDefinition ? type : type.GetGenericTypeDefinition();
-            // LinkedList:暂时无法支持
-            Type[] gen_types = type.GetGenericArguments();
-            object container = ModelManager.Create(type);
-            long epos = m_stream.Position + (long)m_val;
-            if(type_def == typeof(List<>))
-            {
-                IList list = (IList)container; 
-                while(m_stream.Position < epos)
-                {
-                    ReadTag();
-                    object val = ReadField(gen_types[0]);
-                    list.Add(val);
-                }
-            }
-            else if (
-                type_def == typeof(Dictionary<,>) ||
-                type_def == typeof(SortedDictionary<,>))
-            {
-                IDictionary dict = (IDictionary)container;
-                while(m_stream.Position < epos)
-                {
-                    ReadTag();
-                    object key = ReadField(gen_types[0]);
-                    object val = ReadField(gen_types[1]);
-                    dict.Add(key, val);
-                }
-            }
-            return container;
+            // unity ios 不支持
+            return Activator.CreateInstance(type);
         }
 
-        private void ReadTag()
+        private long suspend(long count)
         {
-            uint flag = (uint)m_stream.ReadByte();
-            m_ext = (flag & 0x80) != 0;
-            // read tag
-            m_tag = flag & 0x60;
-            if (m_tag == 3)
-            {
-                ulong tmp = ReadVariant();
-                m_tag = (uint)(tmp + 2);
-            }
-            ++m_tag;
-            // read data
-            m_val = flag & 0x0F;
-            if((flag & 0x10) != 0)
-            {
-                ulong temp;
-                if(!m_ext)
-                {
-                    temp = ReadVariant();
-                }
-                else if(m_indexs.Count > 0)
-                {
-                    temp = m_indexs[m_indexs.Count - 1];
-                    m_indexs.RemoveAt(m_indexs.Count - 1);
-                }
-                else
-                {
-                    throw new Exception("bad packet");
-                }
-                m_val = (temp << 4) | m_val;
-            }
+            long epos = m_epos;
+            m_epos = m_stream.Position + count;
+            return epos;
+        }
+
+        private void recovery(long epos)
+        {
+            m_epos = epos;
+        }
+
+        private bool Read(byte[] buffer, int count, int offset = 0)
+        {
+            if (m_stream.Position + count > m_epos)
+                return false;
+
+            m_stream.Read(buffer, offset, count);
+            return true;
         }
 
         private ulong ReadVariant()
@@ -216,72 +143,53 @@ namespace proto
             int tmp = 0;
             do
             {
-                if(off >= 64)
+                if (off >= 64)
                     return 0;
                 tmp = m_stream.ReadByte();
                 data |= (ulong)(tmp & 0x7F) << off;
                 off += 7;
-            }while((tmp & 0x80) != 0);
+            } while ((tmp & 0x80) != 0);
             return data;
         }
 
-        private uint decode_group_var(byte[] buff, uint off, uint len)
+        private bool ReadTag()
         {
-            int data = 0;
-            int shift = 0;
-            for(uint i = 0; i < len; ++i)
+            if (EndOfStream)
+                return false;
+
+            uint  flag;
+            uint  tag;
+            ulong val;
+            ulong tmp;
+            bool  ext;
+
+            flag = (uint)m_stream.ReadByte();
+            ext = (flag & 0x80) != 0;
+
+            // parse tag
+            tag = flag & 0x60;
+            if(tag == 3)
             {
-                data |= (int)buff[off + i] << shift;
-                shift += 8;
+                tmp = ReadVariant();
+                tag += (uint)tmp;
+                tag += 2;
             }
-            return (uint)data;
-        }
 
-        private long decodei64(ulong n)
-        {
-            return (long)(n >> 1) ^ (-(long)(n & 1));
-        }
+            tag += 1;
 
-        private double decodef64(ulong n)
-        {
-            return BitConverter.Int64BitsToDouble((long)n);
-        }
-
-        private float decodef32(ulong n)
-        {
-            return BitConverter.ToSingle(BitConverter.GetBytes((int)(uint)n), 0);
-        }
-
-        private object decode(Type type, ulong val)
-        {
-            TypeCode code = Type.GetTypeCode(type);
-            switch (code)
+            // parse value
+            val = flag & 0x0f;
+            if((flag & 0x10) != 0)
             {
-                case TypeCode.Boolean:
-                    return val == 1;
-                case TypeCode.Byte:
-                    return (byte)val;
-                case TypeCode.UInt16:
-                    return (ushort)val;
-                case TypeCode.UInt32:
-                    return (uint)val;
-                case TypeCode.UInt64:
-                    return (ulong)val;
-                case TypeCode.SByte:
-                    return (sbyte)decodei64(val);
-                case TypeCode.Int16:
-                    return (short)decodei64(val);
-                case TypeCode.Int32:
-                    return (int)decodei64(val);
-                case TypeCode.Int64:
-                    return (long)decodei64(val);
-                case TypeCode.Single:
-                    return (float)decodef32(val);
-                case TypeCode.Double:
-                    return (double)decodef64(val);
-                default:
-                    return 0;
+                tmp = ReadVariant();
+                val |= (tmp << 4);
             }
+
+            m_tag = tag;
+            m_val = val;
+            m_ext = ext;
+
+            return true;
         }
     }
 }
--- lib-csharp/Encoder.cs
+++ lib-csharp/Encoder.cs
@@ -1,157 +1,84 @@
 ﻿using System;
 using System.IO;
 using System.Text;
-using System.Reflection;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace proto
 {
-    public class Encoder
+    class Encoder
     {
-        private class TagInfo
-        {
-            public long tpos;
-            public long bpos;
-            public uint size;
-        }
-        private List<TagInfo> m_indexs = new List<TagInfo>();
-        private Stream m_stream;
-        private long m_spos;    // 数据起始位置
+        private MemoryStream m_stream;
+        private int m_tag = 0;
 
-        public Encoder(Stream stream)
+        public Encoder()
+        {
+            m_stream = new MemoryStream();
+        }
+
+        public Encoder(MemoryStream stream)
         {
             m_stream = stream;
-            m_spos = stream.Position;
         }
 
-        public void Encode<T>(T msg)
+        public void Encode(IMessage packet)
         {
-            MetaType meta = ModelManager.GetMeta(typeof(T));
-            if (meta == null)
-                return;
-            // clear
-            m_indexs.Clear();
-            // 预留空间
-            byte[] buff = new byte[13];
-            byte flag;
-            long bpos, ipos, epos;
-            // 预留head
-            m_stream.Write(buff, 0, buff.Length);
-            // 写入msg
-            bpos = m_stream.Position;
-            WritePacket(meta, msg);
-            // 写入index
-            ipos = m_stream.Position;
-            if (m_indexs.Count > 0)
-            {// 应该从后向前写入
-                for (int i = m_indexs.Count - 1; i >= 0; --i)
-                {
-                    TagInfo info = m_indexs[i];
-                    if(info.size > 0)
-                        WriteVariant(info.size);
-                }
-                m_indexs.Clear();
-            }
-            epos = m_stream.Position;
-            // 序列化头部
-            uint len1, len2, len3;
-            uint len = 1;
-            len1 = PackHead(buff, len, epos - bpos);
-            len += len1;
-            len2 = PackHead(buff, len, epos - ipos);
-            len += len2;
-            len3 = PackHead(buff, len, meta.MsgID);
-            len += len3;
-            flag = (byte)((len1 << 5) + (len2 << 2) + len3);
-            buff[0] = flag;
-            m_spos = bpos - len;
-            m_stream.Seek(m_spos, SeekOrigin.Begin);
-            m_stream.Write(buff, 0, (int)len);
-            m_stream.Seek(m_spos, SeekOrigin.Begin);
+
         }
 
-        private byte PackHead(byte[] buff, uint off, long data)
+        public Encoder Write<T>(T data, int tagOffset = 1)
         {
-            if (data == 0)
-                return 0;
-            byte count = 0;
-            while (data > 0)
-            {
-                buff[off + count++] = (byte)data;
-                data >>= 8;
-            }
-            return count;
+            m_tag += tagOffset;
+            if (WriteField(data, m_tag))
+                m_tag = 0;
+
+            return this;
         }
 
-        private void WritePacket(MetaType meta, object instance)
-        {
-            uint last_tag = 0;
-            foreach (KeyValuePair<uint, MetaField> kv in meta)
-            {
-                MetaField field = kv.Value;
-                object obj = field.GetValue(instance);
-                if (obj == null)
-                    continue;
-                if(CanWrite(obj))
-                {
-                    WriteField(field.Tag - last_tag, obj);
-                    last_tag = field.Tag;
-                }
-            }
-        }
-
-        private bool CanWrite(object obj)
+        private bool WriteField(object obj, int tag)
         {
             Type type = obj.GetType();
             if(type.IsValueType)
             {
-                ulong data = encode(obj);
-                return data != 0;
-            }
-            else if (type == typeof(string))
-            {
-                if ((obj as string).Length == 0)
+                ulong data = proto.Converter.encode(obj);
+                if (data == 0)
                     return false;
-            }
-            else if (type == typeof(MemoryStream))
-            {
-                if ((obj as MemoryStream).Length == 0)
-                    return false;
-            }
-            else if (type.IsGenericType)
-            {
-                if ((obj as ICollection).Count == 0)
-                    return false;
-            }
-            return true;
-        }
-
-        private bool WriteField(uint tag, object obj)
-        {
-            if (obj == null)
-                return false;
-            Type type = obj.GetType();
-            if(type.IsValueType)
-            {// 普通类型
-                ulong data = encode(obj);
                 WriteTag(tag, data, false);
-                return true;
             }
-            else
+            else if(type == typeof(string))
             {
-                // 写入tag
-                int index;
-                WriteBegin(tag, out index);
-                WriteComplex(type, obj);
-                WriteEnd(index);
-                return true;
-            }
-        }
+                string str = obj as string;
+                if (string.IsNullOrEmpty(str))
+                    return false;
 
-        private void WriteComplex(Type type, object obj)
-        {
-            if (type.IsGenericType)
+                byte[] buff = Encoding.UTF8.GetBytes(str);
+                WriteTag(tag, (ulong)buff.Length, true);
+                Write(buff, buff.Length);
+            }
+            else if(type == typeof(MemoryStream))
+            {
+                MemoryStream stream = obj as MemoryStream;
+                if (stream.Length == 0)
+                    return false;
+
+                byte[] buff = stream.GetBuffer();
+                WriteTag(tag, (ulong)buff.Length, true);
+                Write(buff, buff.Length);
+            }
+            else if(typeof(IMessage).IsAssignableFrom(type))
+            {
+                IMessage msg = obj as IMessage;
+                MemoryStream stream = new MemoryStream();
+                Encoder encoder = new Encoder(stream);
+                msg.Encode(encoder);
+                if (stream.Length == 0)
+                    return false;
+
+                byte[] buff = stream.GetBuffer();
+                WriteTag(tag, (ulong)buff.Length, true);
+                Write(buff, buff.Length);
+            }
+            else if(type.IsGenericType)
             {
                 int count = type.GetGenericArguments().Length;
                 // 一个一个写入
@@ -173,47 +100,96 @@ namespace proto
                     }
                 }
             }
-            else if(type == typeof(string))
+            else
             {
-                byte[] buff = Encoding.UTF8.GetBytes(obj as string);
-                m_stream.Write(buff, 0, buff.Length);
+                throw new Exception("bad type");
             }
-            else if(type == typeof(MemoryStream))
-            {
-                MemoryStream stream = obj as MemoryStream;
-                byte[] buff = stream.GetBuffer();
-                m_stream.Write(buff, 0, (int)stream.Length);
-            }
-            else if(type.IsClass)
-            {
-                MetaType meta = ModelManager.GetMeta(type);
-                if (meta != null)
-                    WritePacket(meta, obj);
-            }
+
+            return true;
         }
 
         private void WriteItem(object obj)
         {
-            if (obj == null)
-                return;
-            // do pack??
-            WriteField(1, obj);
+            Type type = obj.GetType();
+            if (type.IsValueType)
+            {
+                ulong data = proto.Converter.encode(obj);
+                WriteValue(data);
+            }
+            else if (type == typeof(string))
+            {
+                string str = obj as string;
+
+                byte[] buff = Encoding.UTF8.GetBytes(str);
+                WriteValue(buff);
+            }
+            else if (type == typeof(MemoryStream))
+            {
+                MemoryStream stream = obj as MemoryStream;
+                byte[] buff = stream.GetBuffer();
+                WriteValue(buff);
+            }
+            else if (typeof(IMessage).IsAssignableFrom(type))
+            {
+                IMessage msg = obj as IMessage;
+                MemoryStream stream = new MemoryStream();
+                Encoder encoder = new Encoder(stream);
+                msg.Encode(encoder);
+
+                byte[] buff = stream.GetBuffer();
+                WriteValue(buff);
+            }
+            else
+            {
+                throw new Exception("bad type");
+            }
         }
 
-        private void WriteVariant(ulong data)
+        private void WriteValue(ulong length)
         {
-            byte[] buff = new byte[10];
-            int count = 0;
+            byte[] value = new byte[10];
+            int size = EncodeVar(value, 0, length);
+            m_stream.Write(value, 0, size);
+        }
+
+        private void WriteValue(byte[] buff)
+        {
+            int length = buff.Length;
+
+            byte[] value = new byte[10];
+            int size = EncodeVar(value, 0, (ulong)length);
+            Write(value, size);
+            if (length > 0)
+                Write(buff, length);
+        }
+
+        private void WriteTag(int tag, UInt64 val, bool ext)
+        {
+            byte[] data = new byte[20];
+            int size = EncodeTag(data, tag, val, ext);
+            Write(data, size);
+        }
+       
+        private void Write(byte[] data, int size)
+        {
+            m_stream.Write(data, 0, (int)size);
+        }
+
+        private int EncodeVar(byte[] buff, int offset, ulong data)
+        {
+            //外部确保buff足够，int32最多5位，int64最多10位
+            //高位标识：0表示结尾,1表示后边还有数据
+            int count = offset;
             while (data > 0x7F)
             {
                 buff[count++] = (byte)(data & 0x7F | 0x80);
                 data >>= 7;
             }
             buff[count++] = (byte)(data & 0x7F);
-            m_stream.Write(buff, 0, count);
+            return count - offset;
         }
 
-        private void WriteTag(uint tag, ulong val, bool ext)
+        private int EncodeTag(byte[] buff, int tag, ulong val, bool ext)
         {
             byte flag = (byte)(ext ? 0x80 : 0);
             tag -= 1;
@@ -227,107 +203,24 @@ namespace proto
                 flag |= 0x60;
                 tag -= 2;
             }
+
             // 写入低4位
             flag |= (byte)(val & 0x0F);
             val >>= 4;
             if (val > 0)
                 flag |= 0x10;
-            // 写入数据
-            m_stream.WriteByte(flag);
-            // 写入var
+
+            // encode body
+            int length = 1;
+            buff[0] = flag;
+
             if (tag > 0)
-                WriteVariant(tag);
+                length += EncodeVar(buff, length, (ulong)tag);
+
             if (val > 0)
-                WriteVariant(val);
-        }
+                length += EncodeVar(buff, length, val);
 
-        private void WriteBegin(uint tag, out int index)
-        {
-            index = m_indexs.Count;
-            TagInfo info = new TagInfo();
-            info.tpos = m_stream.Position;
-            WriteTag(tag, 0, true);
-            info.bpos = m_stream.Position;
-            m_indexs.Add(info);
-        }
-
-        private void WriteEnd(int index)
-        {
-            TagInfo info = m_indexs[index];
-            long epos = m_stream.Position;
-            long leng = epos - info.bpos;
-            m_stream.Seek(info.tpos, SeekOrigin.Begin);
-            byte flag = (byte)m_stream.ReadByte();
-            flag |= (byte)(leng & 0x0F);
-            leng >>= 4;
-            if(leng > 0)
-            {
-                flag |= 0x10;
-                info.size = (uint)leng;
-            }
-            m_stream.Seek(-1, SeekOrigin.Current);
-            m_stream.WriteByte(flag);
-            m_stream.Seek(epos, SeekOrigin.Begin);
-        }
-
-        private ulong encodei64(long n)
-        {
-            return (ulong)((n << 1) ^ (n >> 63));
-        }
-
-        private ulong encodef64(double n)
-        {
-            return (ulong)BitConverter.DoubleToInt64Bits(n);
-        }
-
-        private ulong encodef32(float n)
-        {
-            return (uint)BitConverter.ToInt32(BitConverter.GetBytes(n), 0);
-        }
-
-        private ulong encode(object obj)
-        {
-            ulong data;
-            TypeCode code = Type.GetTypeCode(obj.GetType());
-            switch (code)
-            {
-                case TypeCode.Boolean:
-                    data = (ulong)((bool)obj ? 1 : 0);
-                    break;
-                case TypeCode.Byte:
-                    data = (byte)obj;
-                    break;
-                case TypeCode.UInt16:
-                    data = (ushort)obj;
-                    break;
-                case TypeCode.UInt32:
-                    data = (uint)obj;
-                    break;
-                case TypeCode.UInt64:
-                    data = (ulong)obj;
-                    break;
-                case TypeCode.SByte:
-                    data = encodei64((sbyte)obj);
-                    break;
-                case TypeCode.Int16:
-                    data = encodei64((short)obj);
-                    break;
-                case TypeCode.Int32:
-                    data = encodei64((int)obj);
-                    break;
-                case TypeCode.Int64:
-                    data = encodei64((long)obj);
-                    break;
-                case TypeCode.Single:
-                    data = encodef32((float)obj);
-                    break;
-                case TypeCode.Double:
-                    data = encodef64((double)obj);
-                    break;
-                default:
-                    return 0;
-            }
-            return data;
+            return length;
         }
     }
 }
